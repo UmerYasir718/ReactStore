bc4052e14e573706d57506ee7ea977d6183fce07
UmerYasir718 <you@example.com>
2023-12-31 05:28:15 +0500

    Done

---
+++ src/mainComponets/Nav.js
@@ -0,0 +1,115 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "../App.css";
+import logo from "../logo.svg";
+import StateContext from "../myComponets/StateContext";
+export default function Nav() {
+  const [cate, setCate] = useState([]);
+  const { cart } = useContext(StateContext);
+  const { cartValue } = useContext(StateContext);
+
+  const categories = async () => {
+    const category = await fetch(
+      "https://fakestoreapi.com/products/categories"
+    );
+    const response = await category.json();
+    console.log(response);
+    setCate(response);
+  };
+  useEffect(() => {
+    categories();
+  }, []);
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark shadow-5-strong Navbar fixed-top">
+        <div className="container-fluid">
+          <img src={logo} className="App-logo Icon" alt="logo" />
+          <Link className="navbar-brand text-light fw-bold fs-1" to="/">
+            ReactStore
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link text-light fw-bold"
+                  aria-current="page"
+                  to="/"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link text-light fw-bold" to="/About">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle text-light fw-bold"
+                  // to="/Categories"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Categories
+                </Link>
+                <ul
+                  className="dropdown-menu  border border-light"
+                  aria-labelledby="navbarDropdown"
+                >
+                  {cate.map((items) => (
+                    <>
+                      <li>
+                        <Link className="dropdown-item text-light">
+                          {items.toUpperCase()}
+                        </Link>
+                      </li>
+                      <li>
+                        <hr className="border border-light border-1 opacity-100" />
+                      </li>
+                    </>
+                  ))}
+                </ul>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-light  me-2" type="submit">
+                Search
+              </button>
+            </form>
+
+            <div className="nav-item bg-dark p-2 rounded">
+              <Link
+                className="nav-link text-light fw-bold d-flex justify-content-center align-content-center"
+                to="/"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                Cart&nbsp;
+                <span className="badge bg-danger p-2 ">{cartValue}</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
--- src/myComponets/Cart.js
+++ src/myComponets/Cart.js
@@ -1,16 +1,25 @@
-import React, { useContext } from "react";
-import Navbar from "./Navbar";
+import React, { useContext, useEffect } from "react";
+import Nav from "../mainComponets/Nav";
 import StateContext from "./StateContext";
 export default function Cart() {
   const { cart } = useContext(StateContext);
   const { cartValue } = useContext(StateContext);
   const { price } = useContext(StateContext);
+  useEffect(() => {
+    // Add a class to the body tag when the component mounts
+    document.body.classList.add("checkout-page");
+
+    // Remove the class when the component unmounts
+    return () => {
+      document.body.classList.remove("checkout-page");
+    };
+  }, []); // Empty dependency array ensures the effect runs only once when the component mounts
+
   return (
     <div>
-      <Navbar />
-      <div className="d-flex justify-content-center align-content-center">
-        <table class="table">
-
+      <Nav />
+      <div className="d-flex justify-content-center align-content-center mt-5">
+        <table class="container table table-bordered">
           <thead>
             <tr>
               <th className="fs-3" scope="col">ID</th>
@@ -35,8 +44,10 @@ export default function Cart() {
             ))}
           </tbody>
         </table>
-        <h3 className="d-flex justify-content-center align-content-center">Total Price:&nbsp;${price}</h3>
       </div>
+      <h3 className="totalPrice">
+        Total Price:&nbsp;${price}
+      </h3>
     </div>
   );
 }
